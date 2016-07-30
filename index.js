27a19c9224f5354ccf441bd33c46a51d12e431e0
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-29 20:20:15 -0700

    Refactor for easier reading

--- index.js
+++ index.js
@@ -10,27 +10,31 @@ function Decoder () {
     return new Decoder()
   }
 
-  this._firstIndexOctets = []
-  this._firstIndex = false
+  this._firstNumberOctetsArray = []
+  this._firstNumber = false
+
   this._blobsEmitted = 0
 
-  Transform.call(this, {readableObjectMode: true})
+  Transform.call(this, {
+    writableObjectMode: false,
+    readableObjectMode: true
+  })
 }
 
 inherits(Decoder, Transform)
 
 var prototype = Decoder.prototype
 
-prototype._resetCurrentBlob = function (index) {
+prototype._createNewBlob = function (index) {
   this._currentBlob = {
-    index: this._firstIndex + this._blobsEmitted,
+    index: this._firstNumber + this._blobsEmitted,
     length: false,
-    lengthOctets: [],
-    bytesToRead: false,
+    lengthOctetsArray: [],
+    bytesLeftToRead: false,
     crc: false,
-    crcOctets: [],
+    crcOctetsArray: [],
     stream: new PassThrough(),
-    pressingBack: false
+    waitingForDrainEvent: false
   }
 }
 
@@ -49,103 +53,114 @@ prototype._transform = function (chunk, encoding, callback) {
   var offset = 0
   var blob = this._currentBlob
   var toWrite = {}
+
   while (offset < chunk.length) {
     // Read the index number at the start of the file.
-    if (!this._firstIndex) {
-      var indexOctets = this._firstIndexOctets
-      indexOctets.push(chunk[offset])
-      if (indexOctets.length === 4) {
-        this._firstIndex = new Buffer(indexOctets).readUInt32BE()
-        delete this._firstIndexOctets
+    if (!this._firstNumber) {
+      var indexOctetsArray = this._firstNumberOctetsArray
+      indexOctetsArray.push(chunk[offset])
+      if (indexOctetsArray.length === 4) {
+        this._firstNumber = octetsToInteger(indexOctetsArray)
+        delete this._firstNumberOctetsArray
       }
       offset++
     } else {
       // Create fresh state for a new blob.
-      if (blob === undefined || blob.bytesToRead === 0) {
-        this._resetCurrentBlob()
+      var needNewBlob = blob === undefined || blob.bytesLeftToRead === 0
+      if (needNewBlob) {
+        this._createNewBlob()
         blob = this._currentBlob
       }
+
       // Read the length of this blob.
       if (!blob.length) {
-        var lengthOctets = blob.lengthOctets
-        lengthOctets.push(chunk[offset])
-        if (lengthOctets.length === 4) {
-          var length = new Buffer(lengthOctets).readUInt32BE()
+        var lengthOctetsArray = blob.lengthOctetsArray
+        lengthOctetsArray.push(chunk[offset])
+        if (lengthOctetsArray.length === 4) {
+          var length = octetsToInteger(lengthOctetsArray)
           blob.length = length
-          blob.bytesToRead = length
-          delete blob.lengthOctets
+          blob.bytesLeftToRead = length
+          delete blob.lengthOctetsArray
         }
         offset++
+
       // Read the CRC.
       } else if (!blob.crc) {
-        var crcOctets = blob.crcOctets
-        crcOctets.push(chunk[offset])
-        if (crcOctets.length === 4) {
-          blob.crc = new Buffer(crcOctets).readUInt32BE()
-          delete blob.crcOctets
+        var crcOctetsArray = blob.crcOctetsArray
+        crcOctetsArray.push(chunk[offset])
+        if (crcOctetsArray.length === 4) {
+          blob.crc = new Buffer(crcOctetsArray).readUInt32BE()
+          delete blob.crcOctetsArray
           this._emit()
         }
         offset++
+
       // Read blob data.
       } else {
         var index = blob.index
-        var dataChunk = chunk.slice(offset, offset + blob.bytesToRead)
+        var dataChunk = chunk.slice(
+          offset, offset + blob.bytesLeftToRead
+        )
         if (toWrite[index] === undefined) {
           toWrite[index] = {
             blob: blob,
-            chunks: []
+            queueOfChunksToWrite: []
           }
         }
-        toWrite[index].chunks.push(dataChunk)
+        toWrite[index].queueOfChunksToWrite.push(dataChunk)
         var bytesSliced = dataChunk.length
-        blob.bytesToRead -= bytesSliced
+        blob.bytesLeftToRead -= bytesSliced
         offset += bytesSliced
       }
     }
   }
 
-  var indices = Object.keys(toWrite)
-  if (indices.length === 0) {
+  var blobsWithQueuedChunks = Object.keys(toWrite)
+  if (blobsWithQueuedChunks.length === 0) {
     callback()
   } else {
-    asyncEach(indices, writeQueuedChunks, callback)
+    asyncEach(blobsWithQueuedChunks, writeQueuedChunks, callback)
   }
 
-  function writeQueuedChunks (index, done) {
+  function writeQueuedChunks (index, doneWritingChunks) {
     var element = toWrite[index]
-    var queue = element.chunks
+    var queue = element.queueOfChunksToWrite
     var blob = element.blob
-    asyncEach(
-      queue,
-      function (chunk, done) {
-        if (blob.pushingBack) {
-          blob.stream.once('drain', function () {
-            write(done)
-          })
-        } else {
-          write(done)
+    var stream = blob.stream
+
+    asyncEach(queue, writeChunkToStream, function (error) {
+      if (error) {
+        doneWritingChunks(error)
+      } else {
+        if (blob.bytesLeftToRead === 0) {
+          stream.end()
         }
-        function write (done) {
-          var readyForMore = blob.stream.write(chunk)
-          blob.pushingBack = !readyForMore
-          if (!readyForMore) {
-            blob.stream.once('drain', function () {
-              blob.pushingBack = false
-            })
-          }
-          done()
-        }
-      },
-      function (error) {
-        if (error) {
-          done(error)
-        } else {
-          if (blob.bytesToRead === 0) {
-            blob.stream.end()
-          }
-          done()
-        }
+        doneWritingChunks()
       }
-    )
+    })
+
+    function writeChunkToStream (chunk, doneWritingChunk) {
+      if (blob.waitingForDrainEvent) {
+        stream.once('drain', function () {
+          writeChunk(doneWritingChunk)
+        })
+      } else {
+        writeChunk(doneWritingChunk)
+      }
+      function writeChunk (doneWritingChunk) {
+        var readyForMoreData = stream.write(chunk)
+        blob.waitingForDrainEvent = !readyForMoreData
+        if (!readyForMoreData) {
+          stream.once('drain', function () {
+            blob.waitingForDrainEvent = false
+          })
+        }
+        doneWritingChunk()
+      }
+    }
   }
+}
+
+function octetsToInteger (octetsArray) {
+  return new Buffer(octetsArray).readUInt32BE()
 }
